8669583bd7f5fae50f16000b0acbdb8fee7e2f52
cluyeye <coxebernardo@outlook.com>
2020-08-20 01:11:46 +0100

    save db connection and CRUD application

---
+++ server/index.js
@@ -0,0 +1,21 @@
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+
+
+// Midleware 
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+app.use('/api/posts', posts);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server started pn port"));
---
+++ server/routes/api/posts.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const mongodb = require('mongodb');
+const { Router } = require('express');
+
+const router = express.Router();
+
+
+// Get Posts
+router.get('/', (req, res) => {
+    res.send('hello');
+});
+
+
+// Add Post
+
+// Delete Post
+
+
+
+
+module.exports = router;
